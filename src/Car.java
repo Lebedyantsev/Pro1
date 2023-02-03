82ca331117cf13af71877f431efe9bba9b72ae5f
T1595 <@Nc14522>
2023-02-03 19:54:40 +0300

    Учебная программа 030223

--- src/Car.java
+++ src/Car.java
@@ -1,21 +1,36 @@
+import java.util.Random;
+
 public class Car {
     private int speed;
-    private Weel w1,w2,w3,w4;
+    private int time;
+    private int distance;
+    private Random r = new Random();
+    private Weel[] weels = new Weel[4];
+
     public Car(int speed) {
-        this.speed = 0;
-        w1 = new Weel(14, 2.3);
-        w2 = new Weel(14, 2.1);
-        w3 = new Weel(14, 2.2);
-        w4 = new Weel(14, 2.0);
+        this.speed = speed;
+
+        for(int i = 0; i < 4; i++)
+            weels[i] = new Weel(14, 2.3);
     }
     public void speedUp() {
-        speed +=10;
+        speed += 10;
     }
     public void speedDown() {
-        speed -=10;
+        speed -= 10;
     }
     public void printSpeed() {
         System.out.println(speed);
+    }
+    public void step() {
+        for(int i = 0; i < 4; i++) {
+            weels[i].changePress(r.nextDouble());
+            System.out.println("Колесо №" + (i + 1));
+            weels[i].printPress();
+        }
+        distance += speed;
+        time++;
+        System.out.println("Время " + time + " Пройдено " + distance);
     }
 }
 class Weel {
@@ -24,4 +39,16 @@ class Weel {
     public Weel(double d, double p) {
         this.dia = d;
         this.press = p;
-    }	}
+    }
+    public void changePress(double delta) {
+        if(press - delta > 0)
+            press -= delta;
+        else {
+            System.out.println("Колесо спустило. Стоп.");
+            System.exit(0);
+        }
+    }
+    public void printPress() {
+        System.out.println("Давление: " + press);
+    }
+}
